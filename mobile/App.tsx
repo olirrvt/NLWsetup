8b4d7412176100534103f3e105a1c3829486fbff
Taylor Oliveira <taylorso2004@hotmail.com>
2023-02-08 18:44:14 -0300

    Aula Mobile: React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    // No mobile o flex é ativo por padrão
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red'
+  }
 });
