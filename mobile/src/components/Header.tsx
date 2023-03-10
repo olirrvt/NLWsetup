fb28c4205425888598add21b163a5e5f05c92e63
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-03-10 08:41:51 -0300

    Add a home ao mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,9 +1,29 @@
-import { View } from 'react-native';
+// Importação da tag View do React Native
+import { View, TouchableOpacity, Text } from "react-native";
+// Dependecias Icones e SVG
+import { Feather } from "@expo/vector-icons";
+// Importação da logo
 import Logo from "../svg/logo.svg";
+// Importação de Cor do Tailwind CSS
+import colors from "tailwindcss/colors";
 
 export function Header() {
-    return (
-        <View className="w-full flex-row items-center justify-between">
-        </View>
-    )
+  return (
+    <View className="w-full flex-row items-center justify-between">
+        <Logo />
+        <TouchableOpacity
+          activeOpacity={0.7}
+        >
+            <Feather 
+            name="plus" 
+            color={colors.violet[500]} 
+            size={20} 
+            />
+        </TouchableOpacity>
+
+        <Text>
+            Novo
+        </Text>
+    </View>
+  );
 }
