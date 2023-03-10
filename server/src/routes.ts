dcd10b0591372a4f72d8a60e1e1dc832c9d637eb
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-03-10 10:55:42 -0300

    Finalizando Rotas do Backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,14 +9,7 @@ import { prisma } from './lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
 
-    /* 
-        Request =>
-        Body: Corpo da Requisição
-        Params: Parametros das Rotas
-        Query: Filtros depois da "?" 
-    */
-
-    app.post('/habits', async (req, res) => {
+    app.post('/habits', async (req) => {
         
         // Tipagem para o TypeScript e validação
         // Utilizando a biblioteca Zod
@@ -92,6 +85,92 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits
         };
 
+    });
+
+    app.patch('/habits/:id/toggle', async (req) => {
+        // Route params => Parâmetro de identificação.
+
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        });
+
+        const { id } = toggleHabitParams.parse(req.params);
+
+        const today = dayjs().startOf('day').toDate();
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today,
+            }
+        });
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today,
+                }
+            });
+        };
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            }
+        });
+
+        // Remover a marcação de completo
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id,
+                }
+            })
+        } else {
+            // Completar o Hábito
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            });
+        };
+    });
+
+    app.get('/summary', async (req) => {
+
+        // [{ data: 10/03, amount: 5, completed: 1 }, { date: 11/03, amount: }]
+        // Prisma ORM = RAW SQL => SQLite
+        // Query mais complexa, mais condições, relacionamentos => SQL na mão (RAW)
+
+        const summary = await prisma.$queryRaw`
+            SELECT 
+            D.id, 
+            D.date,
+            (
+                SELECT 
+                cast(count(*) as float)
+                FROM day_habits DH
+                WHERE DH.day_id = D.id
+            ) as completed,
+            (
+                SELECT
+                cast(count(*) as float)
+                FROM habit_week_days HWD
+                JOIN habits H
+                    ON H.id = HWD.habit_id
+                WHERE
+                  HWD.week_day = cast(strftime('%w', D.date / 1000.0, 'unixepoch') as int)
+                  AND H.created_at <= D.date
+            ) as amount
+            FROM days D
+        `
+
+        // Epoch Time Stamp => 
+
+        return summary;
     });
 
 };
